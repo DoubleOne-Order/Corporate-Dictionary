a2b3db45ef190523986261966946c3cea236421e
Clinton <clintonrumbani@gmail.com>
2025-06-04 17:37:09 +0300

    Addition

--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -5,41 +5,42 @@ const Services = () => {
         <div className='wrapper'>
             <div className='services'>
                 <h1>Our Services</h1>
+                <h2>We help African professionals and SMEs unlock their potential through powerful CVs, strategic career coaching, and digital business solutions.</h2>
                 <div className="services-container">
                     <div className="service-item">
                         <img src="https://images.pexels.com/photos/5989925/pexels-photo-5989925.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
                         <h3>CV Revamping</h3>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, natus! Voluptatibus officia corrupti adipisci dolor sit inventore et, repellendus perspiciatis reiciendis aut quibusdam soluta dolore? Quaerat, cumque! Adipisci, obcaecati temporibus.</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, natus! Voluptatibus officia corrupti adipisci dolor sit inventore et, repellendus perspiciatis reiciendis aut quibusdam.</p>
+                        <button>View More</button>
+                    </div>
+                    <div className="service-item">
+                        <img src="https://images.pexels.com/photos/5439159/pexels-photo-5439159.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
+                        <h3>CV Revamping</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, natus! Voluptatibus officia corrupti adipisci dolor sit inventore et, repellendus perspiciatis reiciendis aut quibusdam.</p>
                         <button>View More</button>
                     </div>
                     <div className="service-item">
                         <img src="https://images.pexels.com/photos/5989925/pexels-photo-5989925.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
                         <h3>CV Revamping</h3>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, natus! Voluptatibus officia corrupti adipisci dolor sit inventore et, repellendus perspiciatis reiciendis aut quibusdam soluta dolore? Quaerat, cumque! Adipisci, obcaecati temporibus.</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, natus! Voluptatibus officia corrupti adipisci dolor sit inventore et, repellendus perspiciatis reiciendis aut quibusdam.</p>
                         <button>View More</button>
                     </div>
                     <div className="service-item">
                         <img src="https://images.pexels.com/photos/5989925/pexels-photo-5989925.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
                         <h3>CV Revamping</h3>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, natus! Voluptatibus officia corrupti adipisci dolor sit inventore et, repellendus perspiciatis reiciendis aut quibusdam soluta dolore? Quaerat, cumque! Adipisci, obcaecati temporibus.</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, natus! Voluptatibus officia corrupti adipisci dolor sit inventore et, repellendus perspiciatis reiciendis aut quibusdam.</p>
                         <button>View More</button>
                     </div>
                     <div className="service-item">
                         <img src="https://images.pexels.com/photos/5989925/pexels-photo-5989925.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
                         <h3>CV Revamping</h3>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, natus! Voluptatibus officia corrupti adipisci dolor sit inventore et, repellendus perspiciatis reiciendis aut quibusdam soluta dolore? Quaerat, cumque! Adipisci, obcaecati temporibus.</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, natus! Voluptatibus officia corrupti adipisci dolor sit inventore et, repellendus perspiciatis reiciendis aut quibusdam.</p>
                         <button>View More</button>
                     </div>
                     <div className="service-item">
                         <img src="https://images.pexels.com/photos/5989925/pexels-photo-5989925.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
                         <h3>CV Revamping</h3>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, natus! Voluptatibus officia corrupti adipisci dolor sit inventore et, repellendus perspiciatis reiciendis aut quibusdam soluta dolore? Quaerat, cumque! Adipisci, obcaecati temporibus.</p>
-                        <button>View More</button>
-                    </div>
-                    <div className="service-item">
-                        <img src="https://images.pexels.com/photos/5989925/pexels-photo-5989925.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
-                        <h3>CV Revamping</h3>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, natus! Voluptatibus officia corrupti adipisci dolor sit inventore et, repellendus perspiciatis reiciendis aut quibusdam soluta dolore? Quaerat, cumque! Adipisci, obcaecati temporibus.</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, natus! Voluptatibus officia corrupti adipisci dolor sit inventore et, repellendus perspiciatis reiciendis aut quibusdam.</p>
                         <button>View More</button>
                     </div>
                 </div>
