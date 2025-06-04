319b3c3458177001ca6a45f1c3a9183482a8c4cf
Clinton <clintonrumbani@gmail.com>
2025-06-04 22:54:42 +0300

    add

--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -8,38 +8,38 @@ const Services = () => {
                 <h2>We help African professionals and SMEs unlock their potential through powerful CVs, strategic career coaching, and digital business solutions.</h2>
                 <div className="services-container">
                     <div className="service-item">
-                        <img src="https://images.pexels.com/photos/5989925/pexels-photo-5989925.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
-                        <h3>CV Revamping</h3>
+                        <img src="https://images.unsplash.com/photo-1616469829581-73993eb86b02?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8bGlua2VkJTIwaW58ZW58MHx8MHx8fDA%3D" alt="" />
+                        <h3>CV Revamp & LinkedIn Optimization</h3>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, natus! Voluptatibus officia corrupti adipisci dolor sit inventore et, repellendus perspiciatis reiciendis aut quibusdam.</p>
                         <button>View More</button>
                     </div>
                     <div className="service-item">
                         <img src="https://images.pexels.com/photos/5439159/pexels-photo-5439159.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
-                        <h3>CV Revamping</h3>
+                        <h3>Interview Prep & Job Search Support</h3>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, natus! Voluptatibus officia corrupti adipisci dolor sit inventore et, repellendus perspiciatis reiciendis aut quibusdam.</p>
                         <button>View More</button>
                     </div>
                     <div className="service-item">
-                        <img src="https://images.pexels.com/photos/5989925/pexels-photo-5989925.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
-                        <h3>CV Revamping</h3>
+                        <img src="https://images.unsplash.com/photo-1528901166007-3784c7dd3653?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Ym9vdGNhbXB8ZW58MHx8MHx8fDA%3D" alt="" />
+                        <h3>Remote Job Bootcamp</h3>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, natus! Voluptatibus officia corrupti adipisci dolor sit inventore et, repellendus perspiciatis reiciendis aut quibusdam.</p>
                         <button>View More</button>
                     </div>
                     <div className="service-item">
-                        <img src="https://images.pexels.com/photos/5989925/pexels-photo-5989925.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
-                        <h3>CV Revamping</h3>
+                        <img src="https://images.unsplash.com/photo-1653669486393-caa6a09ff771?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8Y2FyZWVyJTIwY291bnNlbGxpbmd8ZW58MHx8MHx8fDA%3D" alt="" />
+                        <h3>Career Counseling & Pivot Guidance</h3>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, natus! Voluptatibus officia corrupti adipisci dolor sit inventore et, repellendus perspiciatis reiciendis aut quibusdam.</p>
                         <button>View More</button>
                     </div>
                     <div className="service-item">
-                        <img src="https://images.pexels.com/photos/5989925/pexels-photo-5989925.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
-                        <h3>CV Revamping</h3>
+                        <img src="https://images.unsplash.com/photo-1636231945376-3d40fdcbc462?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fGdyYWR1YXRpb258ZW58MHx8MHx8fDA%3D" alt="" />
+                        <h3>Higher Education Guidance</h3>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, natus! Voluptatibus officia corrupti adipisci dolor sit inventore et, repellendus perspiciatis reiciendis aut quibusdam.</p>
                         <button>View More</button>
                     </div>
                     <div className="service-item">
-                        <img src="https://images.pexels.com/photos/5989925/pexels-photo-5989925.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
-                        <h3>CV Revamping</h3>
+                        <img src="https://images.unsplash.com/photo-1611744314001-6f555577e8f4?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fHNraWxsc3xlbnwwfHwwfHx8MA%3D%3D" alt="" />
+                        <h3>Quick Skill Equipping</h3>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, natus! Voluptatibus officia corrupti adipisci dolor sit inventore et, repellendus perspiciatis reiciendis aut quibusdam.</p>
                         <button>View More</button>
                     </div>
